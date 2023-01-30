bafd6ae69a6147d8d6e8f690dd4e602a28588c74
HowkaCoder <prostojproger@gmail.com>
2023-01-31 00:17:14 +0500

    D. CLI init

---
+++ client/curly_octo_system/src/App.jsx
@@ -0,0 +1,19 @@
+import { Navbar , Welcome , Footer , Loader , Transactions , Services } from './components';
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
